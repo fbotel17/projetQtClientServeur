f204f1c5949e194680847f05eab068fcbaef005b
HURTELJoris <jorishurteldu80@gmail.com>
2023-10-16 11:26:31 +0200

    Final server

--- Source/Server/QT SERVER/Server.cpp
+++ Source/Server/QT SERVER/Server.cpp
@@ -1,33 +1,33 @@
 #include "Server.h"
 
 Server::Server(QObject* parent)
-	: QObject(parent)
+    : QObject(parent)
 {
-	server = new QTcpServer(this);
-	QObject::connect(server, SIGNAL(newConnection()), this, SLOT(onServerNewConnection()));
-	server->listen(QHostAddress::AnyIPv4, 6969);
+    server = new QTcpServer(this);
+    QObject::connect(server, SIGNAL(newConnection()), this, SLOT(onServerNewConnection()));
+    server->listen(QHostAddress::AnyIPv4, 6969);
 
 }
 
 void Server::onServerNewConnection()
 {
-	qDebug() << "Un client s'est connecte !";
-	QTcpSocket* client = server->nextPendingConnection();
-	QObject::connect(client, SIGNAL(readyRead()), this, SLOT(onClientReadyRead()));
-	QObject::connect(client, SIGNAL(disconnected()), this, SLOT(onClientDisconnected()));
+    qDebug() << "Un client s'est connecte !";
+    QTcpSocket* client = server->nextPendingConnection();
+    QObject::connect(client, SIGNAL(readyRead()), this, SLOT(onClientReadyRead()));
+    QObject::connect(client, SIGNAL(disconnected()), this, SLOT(onClientDisconnected()));
 }
 
 void Server::onClientDisconnected()
 {
-	QTcpSocket* obj = qobject_cast<QTcpSocket*>(sender());
-	QObject::disconnect(obj, SIGNAL(readyRead()), this, SLOT(onClientReadyRead()));
-	QObject::disconnect(obj, SIGNAL(disconnected()), this, SLOT(onClientDisconnected()));
-	obj->deleteLater();
+    QTcpSocket* obj = qobject_cast<QTcpSocket*>(sender());
+    QObject::disconnect(obj, SIGNAL(readyRead()), this, SLOT(onClientReadyRead()));
+    QObject::disconnect(obj, SIGNAL(disconnected()), this, SLOT(onClientDisconnected()));
+    obj->deleteLater();
 }
 
 void Server::onClientReadyRead()
 {
-    
+
     QTcpSocket* client = qobject_cast<QTcpSocket*>(sender());
 
     // Si il y a un client.
@@ -50,7 +50,7 @@ void Server::onClientReadyRead()
 
         QRegularExpression ExpHr("^Hr\\d{2}\\?$");
         QRegularExpressionMatch Hr = ExpHr.match(request);
-     
+
 
         // Si la requête du client corespond à une des trois expressions.
 
@@ -59,19 +59,41 @@ void Server::onClientReadyRead()
 
             // Générez la température aléatoire en degrés Celsius de -20.0 degré à 40.0 degrès.
             srand(time(NULL));
-            float temperatureCelsius = rand() % 6000 - 2000;
-            temperatureCelsius /= 100.0;
+            // Générer des nombres aléatoires pour chaque unité
+            float centiemes = rand() % 10;     // Entre 0 et 9
+            float diziemes = rand() % 10;      // Entre 0 et 9
+            float dizaines = rand() % 6;        // Entre 0 et 6
+            float unites = rand() % 10;         // Entre 0 et 9
+
+            // Additionner les unités et diviser par 100 pour obtenir la température
+            double temperatureCelsius = unites + dizaines * 10 + diziemes / 10 + centiemes / 100;
+          
+
+            // Ajuster la plage de -20.00 à 40.00
+            temperatureCelsius = -20.00 + temperatureCelsius;
+            
 
             // Convertissez la température en degrés Fahrenheit
             float temperatureFahrenheit = (temperatureCelsius * 9 / 5) + 32;
 
             // Si on demande la température en degré Celcius.
             if (request.startsWith("Td")) {
-                response = QString("Td%1,Y%2").arg(sensorNumber).arg(temperatureCelsius, 0, 'f', 2);
+                if (temperatureCelsius >= 0.00) {
+                    response = QString("Td%1,+%2").arg(sensorNumber).arg(temperatureCelsius, 0, 'f', 2);
+                }
+                else {
+                    response = QString("Td%1,%2").arg(sensorNumber).arg(temperatureCelsius, 0, 'f', 2);
+                }
             }
             // Si on demande la température en degré Farhenheit.
             else if (request.startsWith("Tf")) {
-                response = QString("Tf%1,Y%2").arg(sensorNumber).arg(temperatureFahrenheit, 0, 'f', 2);
+                if (temperatureFahrenheit >= 0.00) {
+                    response = QString("Tf%1,%2").arg(sensorNumber).arg(temperatureFahrenheit, 0, 'f', 2);
+                }
+                else {
+                    response = QString("Tf%1,%2").arg(sensorNumber).arg(temperatureFahrenheit, 0, 'f', 2);
+                }
+                
             }
             // Si on demande l'hydromédtrie.
             else if (request.startsWith("Hr")) {
@@ -92,5 +114,6 @@ void Server::onClientReadyRead()
 }
 
 Server::~Server() {
-	delete(server);
+    delete(server);
 }
+
